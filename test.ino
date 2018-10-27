9017f642eeae376b960d3db8ecd239d0bb298adb
Timon Paßlick <timon.passlick@gmx.de>
2018-10-27 11:40:14 +0200

    Added tests for constexpr arrays

--- test.ino
+++ test.ino
@@ -1,31 +1,38 @@
-#include <array_lib.h>
+#include "array_lib.h"
 #include <assert.h>
 
 void setup() {
-  stack_array<int, 3> a{2, 4, 6};
-  assert(a[0] == 2);
-  assert(a[1] == 4);
-  assert(a[2] == 6);
-  assert(a.length() == 3);
-  assert(length(a.c_array) == 3);
-
-  heap_array<int> b{3};
-  b[0] = 2;
-  b[1] = 4;
-  b[2] = 6;
+  constexpr StackArray<int, 3> a{2, 4, 6};
+  static_assert(a[0] == 2, "a[0] is not 2");
+  static_assert(a[1] == 4, "a[1] is not 4");
+  static_assert(a[2] == 6, "a[2] is not 6");
+  static_assert(a.length() == 3, "a has not 3 elements");
+  static_assert(length(a.c_array) == 3, "a has 3 elements but not its C array");
+  
+  StackArray<int, 3> b{2, 4, 6};
   assert(b[0] == 2);
   assert(b[1] == 4);
   assert(b[2] == 6);
   assert(b.length() == 3);
-  
-  growing_array<int> c;
-  c.push(2);
-  c.push(4);
-  c.push(6);
+  assert(length(b.c_array) == 3);
+
+  HeapArray<int> c{3};
+  c[0] = 2;
+  c[1] = 4;
+  c[2] = 6;
   assert(c[0] == 2);
   assert(c[1] == 4);
   assert(c[2] == 6);
   assert(c.length() == 3);
+  
+  GrowingArray<int> d;
+  d.push(2);
+  d.push(4);
+  d.push(6);
+  assert(d[0] == 2);
+  assert(d[1] == 4);
+  assert(d[2] == 6);
+  assert(d.length() == 3);
 }
 
 void loop() {
